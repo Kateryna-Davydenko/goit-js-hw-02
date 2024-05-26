f2cec48b230191ff67dc6746b39506bd4ba08489
Kateryna Davydenko <davidenko290907@gmail.com>
2024-05-26 21:48:10 +0300

    main

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,13 @@
-function checkForSpam(message) {}
+function checkForSpam(message) {
+  if (
+    message.toLowerCase().includes("spam") ||
+    message.toLowerCase().includes("sale")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
